5d64fa1bc69f33271ecf565577b75ac7e78a0402
RIDHIKA JOSHI <92508014+RidhikaJoshi@users.noreply.github.com>
2023-09-20 23:45:11 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,14 +1,1 @@
-def calculate_sum(a, b):
-    """
-    Calculate the sum of two numbers.
-
-    :param a: The first number.
-    :param b: The second number.
-    :return: The sum of the two numbers.
-    """
-    return a + b
-
-# Example usage:
-result = calculate_sum(5, 3)
-print("Result:", result)
-
+print("Hello, World!")
